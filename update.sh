b444978ef963a3673cfe7deed1b316dd3420e520
LEO STRIETZEL <leostrietzel@gmail.com>
2025-07-02 12:18:30 +0200

    Quartz sync: Jul 2, 2025, 12:18 PM

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SRC_FOLDER="/Users/leostrietzel/Vaults/Leo's vault/Einführung in die KI"
+DST_FOLDER="/Users/leostrietzel/Documents/Projekte/Codes/quartz/content"
+
+if [ ! -d "$SRC_FOLDER" ]; then
+  echo "Error: Source folder does not exist."
+  exit 1
+fi
+
+# Check if the destination folder exists
+if [ -d "$DST_FOLDER" ]; then
+  echo "Destination will be overwritten."
+fi
+
+# Copy the folder
+cp -r "$SRC_FOLDER" "$DST_FOLDER"
+
+echo "Folder copied successfully."
+
+echo "updating quartz content"
+npx quartz sync
